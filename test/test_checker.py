6fd4e2be1c0f87d95184b632d3cd90a5ce28d126
Mi Yu <miyu.china@icloud.com>
2018-01-17 11:13:57 -0600

    add test for checker

---
+++ test/test_checker.py
@@ -0,0 +1,111 @@
+from unittest import TestCase
+from unittest.mock import patch
+from gone.checker import CheckProgramVisitor
+from gone.parser import parse
+
+class TestChecker(TestCase):
+    def setUp(self):
+        self.checker = CheckProgramVisitor()
+        self.check_program = lambda source: self.checker.visit(parse(source))
+        self.captured_output = []
+        self.patcher = patch('builtins.print', self.mock_print)
+        self.patcher.start()
+        self.addCleanup(self.patcher.stop)
+
+    # checktest0
+    def test_track_symbols(self):
+        source = ("const pi = 3.14159;\n"
+                  "var x int;\n"
+                  "\n"
+                  "print pi;\n"
+                  "print x;\n"
+                  "print y;\n"
+                  "\n"
+                  "x = 45;\n"
+                  "z = 13;\n")
+        self.check_program(source)
+        for symbol in ('pi', 'x'):
+            self.assertTrue(symbol in self.checker.symbols)
+        expected_output = [('6: NameError: symbol "y" undefined.',),
+                           ('9: NameError: symbol "z" undefined.',),
+                           ('9: TypeError: assigning type int to "z" of type error',)]
+        self.assertEqual(expected_output, self.captured_output)
+
+    # checktest1
+    def test_redefinition(self):
+        source = ("const a = 2;        // OK\n"
+                  "var x int;          // OK\n"
+                  "\n"
+                  "var a float;        // ERROR. a previously defined\n"
+                  "const x = 3;        // ERROR. x previously defined\n")
+        self.check_program(source)
+        expected_output = [('4: NameError: variable "a" already defined.',),
+                           ('5: NameError: constant "x" already defined.',)]
+        self.assertEqual(expected_output, self.captured_output)
+
+    # checktest2
+    def test_read_write(self):
+        source = ("const a = 2;\n"
+                  "a = 4;              // ERROR. a is constant\n"
+                  "print a;            // OK. Reading\n"
+                  "\n"
+                  "var b int;\n"
+                  "b = 5;              // OK. Storing a value\n"
+                  "print b;            // OK. Reading\n")
+        self.check_program(source)
+        expected_output = [('2: TypeError: cannot assign to constant "a"',),]
+        self.assertEqual(expected_output, self.captured_output)
+
+    # checktest5
+    def test_operation_types(self):
+        source = ("print 2 + 3.5;    // ERROR\n"
+                  "print 2.0 + 3;    // ERROR\n"
+                  "\n"
+                  "print 'h' + 'w';  // ERROR\n"
+                  "print 'h' - 'w';\n"
+                  "print 'h' * 'w';\n"
+                  "print 'h' / 'w';\n"
+                  "print -'h';\n"
+                  "print +'h';\n")
+        self.check_program(source)
+        expected_output = [('1: TypeError: performing "+" on int and float',),
+                           ('2: TypeError: performing "+" on float and int',),
+                           ('4: TypeError: performing "+" on char and char',),
+                           ('5: TypeError: performing "-" on char and char',),
+                           ('6: TypeError: performing "*" on char and char',),
+                           ('7: TypeError: performing "/" on char and char',),
+                           ('8: TypeError: performing "-" on char',),
+                           ('9: TypeError: performing "+" on char',)]
+        self.assertEqual(expected_output, self.captured_output)
+
+    # checktest6
+    def test_declare_types(self):
+        source = ("const a = 1;\n"
+                  "var x int;\n"
+                  "\n"
+                  "x = a + 2;        // OK\n"
+                  "x = 3.5;          // Error\n"
+                  "\n"
+                  "var y int = 3.5;  // Error\n"
+                  "var z spam;       // Error. Unknown type name 'spam'\n")
+        self.check_program(source)
+        expected_output = [('5: TypeError: assigning type float to "x" of type int',),
+                           ('7: TypeError: assigning type float to "y" of type int',),
+                           ('8: TypeError: unknown type "spam"',)]
+        self.assertEqual(expected_output, self.captured_output)
+
+    # checktest7
+    def test_builtin_types(self):
+        source = ("print float;      // ERROR. float is not a valid expression\n"
+                  "int = 3;          // ERROR. int is not a valid location\n"
+                  "var int float;    // ERROR. int previously declared (as a type)\n")
+        self.check_program(source)
+        expected_output = [('1: NameError: symbol "float" undefined.',),
+                           ('2: NameError: symbol "int" undefined.',),
+                           ('2: TypeError: assigning type int to "int" of type error',),
+                           ('3: NameError: cannot declare variable with name int',)]
+        self.assertEqual(expected_output, self.captured_output)
+
+    def mock_print(self, *args, **kwargs):
+        self.captured_output.append(args)
+
